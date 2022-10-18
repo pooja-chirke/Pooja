efaa1ec54889d95c033350bac349a4f7ca7d10ac
pooja <poojachirke293@gmail.com>
2022-10-18 00:38:46 -0700

    Web Update

---
+++ sum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,sum;
+	cout<<"enter the first number\n";
+	cin>>a;
+	cout<<"enter the second number\n";
+	cin>>b;
+	sum = a+b;
+	cout<<"sum=\n";
+	cout<<sum;
+	return 0;
+	
+}
